ec80b5c150217390a9122ce2710c91f4b1f9b2b9
bercat <catholm1@outlook.com>
2024-06-05 20:02:30 +0200

    made the celles and the basic style

--- app.js
+++ app.js
@@ -1,2 +1,25 @@
-const gameBoard = document.querySelector("gameboard")
-console.log(gameBoard)
+const gameBoard = document.querySelector("#gameboard")
+
+const infoDisplay = document.querySelector("#info")
+
+const startCells = [
+    "", "", "",
+    "", "", "",
+    "", "", ""
+]
+
+
+infoDisplay.textContent = "Circle goes first"
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement("div")
+        cellElement.classList.add("square")
+        cellElement.innerHTML = index
+        gameBoard.append(cellElement)
+    })
+}
+
+createBoard()
+
+console.log(infoDisplay)
