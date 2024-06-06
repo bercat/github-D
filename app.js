587bedaadd658e801c4433231c8ea227b0af5e36
bercat <catholm1@outlook.com>
2024-06-06 13:42:57 +0200

    last commit

--- app.js
+++ app.js
@@ -8,18 +8,44 @@ const startCells = [
     "", "", ""
 ]
 
+let go = "circle"
+
 
 infoDisplay.textContent = "Circle goes first"
 
 function createBoard() {
-    startCells.forEach((cell, index) => {
+    startCells.forEach((_cell, index) => {
         const cellElement = document.createElement("div")
         cellElement.classList.add("square")
-        cellElement.innerHTML = index
+        //make a id from 1 to 9
+        cellElement.id = index
+
+        //Add a event with addGO every time a click happen on cellElement
+        cellElement.addEventListener("click", addGo)
+
+
         gameBoard.append(cellElement)
     })
 }
 
 createBoard()
 
-console.log(infoDisplay)
+function addGo(e) {
+    console.log("clicked", e);
+    
+    const goDisplay = document.createElement("div")
+    goDisplay.classList.add("go")
+    //e.target.append(goDisplay)
+
+}
+
+addGo()
+
+
+//const circleElement = document.createElement("div")
+//circleElement.classList.add("cross")
+
+//adding the circle element to cellElement
+//cellElement.append(circleElement)
+
+//cellElement.innerHTML = index
