f984cacfb29dd13df770bd0b103221ad1aa98c4f
bercat <catholm1@outlook.com>
2024-06-13 09:05:39 +0200

    setting the circle

--- app.js
+++ app.js
@@ -30,16 +30,29 @@ function createBoard() {
 
 createBoard()
 
+/*This setup ensures that when the button 
+with id 0 to 9 is clicked, a new div 
+with the class go will be created and 
+appended to the button.
+*/
+
 function addGo(e) {
-    console.log("clicked", e);
+    
+    //console.log("clicked", e.target);
     
     const goDisplay = document.createElement("div")
-    goDisplay.classList.add("go")
-    //e.target.append(goDisplay)
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+
+    /*reassigning go`s value.
+    the value of go alternates 
+    between "circle" and "cross" */
+    go = go === "circle" ? "cross" : "circle"
+    console.log(go)
 
 }
 
-addGo()
+//addGo()
 
 
 //const circleElement = document.createElement("div")
